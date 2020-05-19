591f010758136e7318838dacbd78d081c0f026c7
Antonio Stanila <antoniostanila98@gmail.com>
2020-05-19 20:24:18 +0300

    tv

--- app/src/main/java/com/example/ioc/MainActivity.java
+++ app/src/main/java/com/example/ioc/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.air_conditioner);
+        setContentView(R.layout.televizor);
     }
 }
