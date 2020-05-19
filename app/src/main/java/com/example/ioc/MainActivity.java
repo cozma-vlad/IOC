f89ca4b2d63446f97e817c7f384c803e19ee1087
student <stiuca.nicusor@gmail.com>
2020-05-20 01:05:39 +0300

    floatingbutton

--- app/src/main/java/com/example/ioc/MainActivity.java
+++ app/src/main/java/com/example/ioc/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.ioc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Item> itemList=new ArrayList<>();
+    ArrayList<Item> inactiveList=new ArrayList<>();
+    private int listselector=1;
     public static ItemAdapter itemAdapter;
 
     public void init(){
@@ -20,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
         itemList.add(new Item("Television",1));
         itemList.add(new Item("Air Conditioner",2));
         itemList.add(new Item("Lights",4));
+        itemList.add(new Item("Hood",3));
+        itemList.add(new Item("Television",1));
+        itemList.add(new Item("Air Conditioner",2));
+        itemList.add(new Item("Lights",4));
+        itemList.add(new Item("Hood",3));
+        itemList.add(new Item("Television",1));
+        itemList.add(new Item("Air Conditioner",2));
+        itemList.add(new Item("Lights",4));
+        itemList.add(new Item("Hood",3));
+        itemList.add(new Item("Television",1));
+        itemList.add(new Item("Air Conditioner",2));
+        itemList.add(new Item("Lights",4));
+        itemList.add(new Item("Hood",3));
+        itemList.add(new Item("Television",1));
+        itemList.add(new Item("Air Conditioner",2));
+        itemList.add(new Item("Lights",4));
+
+
+        inactiveList.add(new Item("Hood",3));
+        inactiveList.add(new Item("Television",1));
+        inactiveList.add(new Item("Air Conditioner",2));
+        inactiveList.add(new Item("Lights",4));
     }
 
     @Override
@@ -29,6 +56,29 @@ public class MainActivity extends AppCompatActivity {
         init();
 
         itemAdapter=new ItemAdapter(itemList,this);
+
+        final FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListView listView=(ListView)findViewById(R.id.ItemListView);
+                if(listselector==1)
+                {
+                    itemAdapter.ItemList=inactiveList;
+                    listView.setBackgroundColor(getResources().getColor(R.color.unnactive_color));
+                    itemAdapter.notifyDataSetChanged();
+                    fab.setImageResource(R.drawable.ic_close_black_24dp);
+                    listselector=2;
+                }else{
+                    itemAdapter.ItemList=itemList;
+                    listView.setBackgroundColor(getResources().getColor(R.color.default_color));
+                    itemAdapter.notifyDataSetChanged();
+                    fab.setImageResource(R.drawable.ic_add_black_24dp);
+                    listselector=1;
+
+                }
+            }
+        });
 
         ListView listView=(ListView)findViewById(R.id.ItemListView);
         listView.setAdapter(itemAdapter);
